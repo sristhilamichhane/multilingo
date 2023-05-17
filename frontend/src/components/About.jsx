8afb8e759ea46a8fe78621ed662afb6ef4cd42da
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-05-17 09:35:07 +0545

    feat: :sparkles: About component

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import Sidebar from "../Sidebar";
+const About = () => {
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <h1>About</h1>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default About;
