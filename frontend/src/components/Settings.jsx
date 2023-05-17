78cd25c6be091ff24d1f8e0bacdbb560c29c07d4
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-05-17 09:35:46 +0545

    feat: :sparkles: introducing setting component

---
+++ frontend/src/components/Settings.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import Sidebar from "../Sidebar";
+const Settings = () => {
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <h1>Settings</h1>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Settings;
