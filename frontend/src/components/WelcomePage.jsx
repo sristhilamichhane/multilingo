26824c0dc2d7ac47965341859bf32a29d1adbf3b
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-05-29 07:11:35 +0545

    build: :art: welcome section added

--- frontend/src/components/WelcomePage.jsx
+++ frontend/src/components/WelcomePage.jsx
@@ -1,21 +1,128 @@
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-const WelcomePage = () => {
-  return (
-    <div className="carousel-wrapper">
-      <Carousel infiniteLoop useKeyboardArrows autoPlay>
-        <div>
-          <img src="../assets/hello.png" />
-        </div>
-        <div>
-          <img src="../assets/hello2.png" />
-        </div>
-        <div>
-          <img src="../assets/hello3.png" />
-        </div>
-      </Carousel>
-    </div>
-  );
-};
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
 
-export default WelcomePage;
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: "शान्ति मुस्कानबाट सुरु हुन्छ ।  ",
+    imgPath:
+      "https://www.kullabs.org/uploads/20150106living-together-600x0.jpg",
+  },
+  {
+    label: "जस्तो देश उस्तै भेष ",
+    imgPath:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJvxpnr3PmrDUq4Qr9z6McS_XaE_1B1f92Kw&usqp=CAU",
+  },
+  {
+    label: "सय रोग एउटै दबाइ हाँस्न सिक्नु हो ।",
+    imgPath:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7-HN-kAm21XiuHCq-HZsa2LrLiblFM6EbQksSz7Eq&s",
+  },
+  {
+    label: "शुद्ध मन नम्बरी सुनभन्दा धेरै मुल्यवान हुन्छ ।",
+    imgPath:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9MP4xJpXB1bq0qXT2b3s0TBNZ8gkL3emoTg&usqp=CAU",
+  },
+];
+
+function SwipeableTextMobileStepper() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          height: 50,
+          pl: 2,
+          bgcolor: "background.default",
+        }}
+      >
+        <Typography>{images[activeStep].label}</Typography>
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 255,
+                  display: "block",
+                  maxWidth: 400,
+                  overflow: "hidden",
+                  width: "100%",
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
+  );
+}
+
+export default SwipeableTextMobileStepper;
