90743c6e8c2f87884bd69d374726333ebbb7f796
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-06-22 07:14:46 +0545

    feat: :sparkles: base of add question for admin

--- frontend/src/Pages/AddQuestion.jsx
+++ frontend/src/Pages/AddQuestion.jsx
@@ -1,18 +1,28 @@
 import Navbar from "../components/Navbar";
-import { Box } from "@mui/material";
+import { Box, styled } from "@mui/material";
 import Sidebar from "../components/Sidebar";
+
+const MainContent = styled(Box)(({ theme }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  [theme.breakpoints.down("sm")]: {
+    padding: theme.spacing(2),
+  },
+}));
 
 const AddQuestion = () => {
   return (
     <>
-      <Navbar />
-      <Box height={30} />
-      <Box sx={{ display: "flex" }}>
-        <Sidebar />
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <h1>Add Questions</h1>
+      <div className="bgColor">
+        <Navbar />
+        <Box height={30} />
+        <Box sx={{ display: "flex" }}>
+          <Sidebar />
+          <MainContent>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}></Box>
+          </MainContent>
         </Box>
-      </Box>
+      </div>
     </>
   );
 };
