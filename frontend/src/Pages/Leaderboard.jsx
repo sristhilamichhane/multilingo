ceb8f696845675b9ca0d48187f05d7df6f154cb8
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-06-22 07:15:13 +0545

    feat: :sparkles: leaderboard alert!

---
+++ frontend/src/Pages/Leaderboard.jsx
@@ -0,0 +1,30 @@
+import { Box, styled } from "@mui/material";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+
+const MainContent = styled(Box)(({ theme }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  [theme.breakpoints.down("sm")]: {
+    padding: theme.spacing(2),
+  },
+}));
+
+const Leaderboard = () => {
+  return (
+    <>
+      <div className="bgColor">
+        <Navbar />
+        <Box height={30} />
+        <Box sx={{ display: "flex" }}>
+          <Sidebar />
+          <MainContent>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}></Box>
+          </MainContent>
+        </Box>
+      </div>
+    </>
+  );
+};
+
+export default Leaderboard;
