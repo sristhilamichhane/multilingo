b4ccb68cbc085f14d8d3a5e52025b2d96046d8f2
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-06-23 08:09:03 +0545

    build: :art: populating leaderboard

--- frontend/src/Pages/Leaderboard.jsx
+++ frontend/src/Pages/Leaderboard.jsx
@@ -1,4 +1,4 @@
-import { Box, styled } from "@mui/material";
+import { Box, styled, Typography, Avatar, Card, Stack } from "@mui/material";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 
@@ -11,6 +11,20 @@ const MainContent = styled(Box)(({ theme }) => ({
 }));
 
 const Leaderboard = () => {
+  const quizResults = [
+    { name: "User 1", avatar: "/avatars/avatar1.png", score: 80 },
+    { name: "User 2", avatar: "/avatars/avatar2.png", score: 95 },
+    { name: "User 3", avatar: "/avatars/avatar3.png", score: 75 },
+    { name: "User 4", avatar: "/avatars/avatar3.png", score: 70 },
+    { name: "User 5", avatar: "/avatars/avatar3.png", score: 90 },
+    { name: "User 6", avatar: "/avatars/avatar3.png", score: 50 },
+    { name: "User 7", avatar: "/avatars/avatar3.png", score: 40 },
+    // Add more quiz results data here
+  ];
+
+  // Sort the quiz results array by score in descending order
+  const sortedResults = [...quizResults].sort((a, b) => b.score - a.score);
+
   return (
     <>
       <div className="bgColor">
@@ -19,7 +33,54 @@ const Leaderboard = () => {
         <Box sx={{ display: "flex" }}>
           <Sidebar />
           <MainContent>
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}></Box>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+              <Typography
+                variant="h4"
+                gutterBottom
+                sx={{
+                  pb: 3,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  color: "blue",
+                }}
+              >
+                Leaderboard
+              </Typography>
+              {sortedResults.map((user, index) => (
+                <Box
+                  key={user.name}
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    marginBottom: 3,
+                  }}
+                >
+                  <Card
+                    sx={{ width: "100%", height: "30%" }}
+                    className="gradientLight"
+                  >
+                    <Stack direction="row" gap={3} p={1}>
+                      <Typography
+                        variant="h6"
+                        sx={{ marginRight: { sm: 1, md: 10 } }}
+                      >
+                        {index + 1}
+                      </Typography>
+                      <Avatar
+                        alt={user.name}
+                        src={user.avatar}
+                        sx={{ marginRight: { sm: 1, md: 10 } }}
+                      />
+                      <Typography>{user.name}</Typography>
+                      <Typography variant="h6" sx={{ marginLeft: "auto" }}>
+                        Score: {user.score}
+                      </Typography>
+                    </Stack>
+                  </Card>
+                </Box>
+              ))}
+            </Box>
           </MainContent>
         </Box>
       </div>
