38031cc1128aadc41d78b7ecddd36c5b09bdabc7
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-06-26 06:25:02 +0545

    feat: :sparkles: AddQuestion of admin

--- frontend/src/Pages/AddQuestion.jsx
+++ frontend/src/Pages/AddQuestion.jsx
@@ -1,6 +1,19 @@
-import Navbar from "../components/Navbar";
 import { Box, styled } from "@mui/material";
+import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import {
+  TextField,
+  Button,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  Grid,
+  Card,
+  CardContent,
+} from "@mui/material";
+import { useState } from "react";
 
 const MainContent = styled(Box)(({ theme }) => ({
   flexGrow: 1,
@@ -11,6 +24,30 @@ const MainContent = styled(Box)(({ theme }) => ({
 }));
 
 const AddQuestion = () => {
+  const [question, setQuestion] = useState("");
+  const [options, setOptions] = useState(["", "", "", ""]);
+  const [correctOption, setCorrectOption] = useState("");
+  const [questions, setQuestions] = useState([]);
+
+  const handleOptionChange = (event, index) => {
+    const updatedOptions = [...options];
+    updatedOptions[index] = event.target.value;
+    setOptions(updatedOptions);
+  };
+
+  const handleAddQuestion = () => {
+    const newQuestion = {
+      question,
+      options,
+      correctOption,
+    };
+
+    setQuestions([...questions, newQuestion]);
+    setQuestion("");
+    setOptions(["", "", "", ""]);
+    setCorrectOption("");
+  };
+
   return (
     <>
       <div className="bgColor">
@@ -19,7 +56,95 @@ const AddQuestion = () => {
         <Box sx={{ display: "flex" }}>
           <Sidebar />
           <MainContent>
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}></Box>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+              <Box>
+                <TextField
+                  label="Question"
+                  value={question}
+                  onChange={(event) => setQuestion(event.target.value)}
+                  fullWidth
+                  multiline
+                  rows={4}
+                  variant="outlined"
+                  margin="normal"
+                />
+                <FormControl component="fieldset" margin="normal">
+                  <FormLabel component="legend">Options</FormLabel>
+                  <Grid container spacing={2} margin={2}>
+                    {options.map((option, index) => (
+                      <Grid item xs={12} sm={6} key={index}>
+                        <TextField
+                          label={`Option ${index + 1}`}
+                          value={option}
+                          onChange={(event) => handleOptionChange(event, index)}
+                          fullWidth
+                          variant="outlined"
+                          margin="normal"
+                        />
+                      </Grid>
+                    ))}
+                  </Grid>
+                </FormControl>
+                <br />
+                <FormControl component="fieldset" margin="normal">
+                  <FormLabel component="legend" sx={{ marginBottom: 2 }}>
+                    Correct Option
+                  </FormLabel>
+                  <RadioGroup
+                    value={correctOption}
+                    onChange={(event) => setCorrectOption(event.target.value)}
+                    row
+                  >
+                    {options.map((option, index) => (
+                      <FormControlLabel
+                        key={index}
+                        value={`${index}`}
+                        control={<Radio />}
+                        label={`Option ${index + 1}`}
+                      />
+                    ))}
+                  </RadioGroup>
+                </FormControl>
+                <br />
+                <Button
+                  variant="contained"
+                  onClick={handleAddQuestion}
+                  sx={{ mt: 2 }}
+                >
+                  Add Question
+                </Button>
+
+                {questions.length > 0 && (
+                  <Box sx={{ marginTop: "1rem" }}>
+                    {questions.map((q, index) => (
+                      <Card key={index} sx={{ marginTop: "1rem" }}>
+                        <CardContent>
+                          <h3>New Question:</h3>
+                          <p>{q.question}</p>
+                          <div>
+                            {q.options.map((option, optionIndex) => (
+                              <Button
+                                key={optionIndex}
+                                variant="outlined"
+                                sx={{
+                                  marginRight: "1rem",
+                                  marginBottom: "1rem",
+                                }}
+                              >
+                                {option}
+                              </Button>
+                            ))}
+                          </div>
+                          <p>
+                            Correct Option: Option {Number(q.correctOption) + 1}
+                          </p>
+                        </CardContent>
+                      </Card>
+                    ))}
+                  </Box>
+                )}
+              </Box>
+            </Box>
           </MainContent>
         </Box>
       </div>
