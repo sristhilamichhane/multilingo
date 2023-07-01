574f25c4d164209972f33eaee1956dce2232ff0c
Md Jiyaul Haq <69068755+MdJiyaulHaq@users.noreply.github.com>
2023-07-01 15:44:25 +0545

    Model User created.

---
+++ backend/users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # managed = False
+        db_table = 'user'
+
+    def __str__(self):
+        return self.username
