7eb60b1593668bb34ca52752365be0354c98e7f7
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-09-14 14:38:13 +0545

    feat: :sparkles: Login page

--- frontend/src/components/SignUpLogin/Login.jsx
+++ frontend/src/components/SignUpLogin/Login.jsx
@@ -1,76 +1,92 @@
+import { useState } from "react";
 import {
-  Container,
-  Grid,
-  TextField,
   Button,
+  TextField,
   Typography,
+  Container,
   Paper,
+  Grid,
+  Link,
 } from "@mui/material";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "../../Pages/Home.css";
 
-const Login = () => {
+function Login() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // You can implement your authentication logic here.
+    // For simplicity, let's assume a hardcoded admin login.
+    if (username === "admin" && password === "adminpassword") {
+      navigate("/AdminHome");
+    } else {
+      navigate("/UserHome");
+    }
+  };
+
   return (
-    <Container maxWidth="xs">
-      <Paper sx={{ padding: 2, marginTop: 10 }}>
-        <Typography variant="h4" align="center" gutterBottom>
-          Login
-        </Typography>
-        <form>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Email Address"
-                name="email"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Password"
-                name="password"
-                type="password"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                fullWidth
-                variant="contained"
-                color="primary"
-                type="submit"
-              >
-                Login
-              </Button>
-            </Grid>
-            <Grid item xs={12}>
-              <Link href="#" variant="body2">
-                Forgot Password?
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      style={{ minHeight: "100vh" }} // Center vertically on the page
+    >
+      <Grid item xs={12} sm={8} md={6} lg={4}>
+        <Paper
+          elevation={3}
+          style={{ padding: "20px" }}
+          className="gradientLight"
+        >
+          <Container>
+            <Typography variant="h4" align="center" gutterBottom>
+              Login
+            </Typography>
+            <TextField
+              label="Username"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              label="Password"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <hr />
+            <br />
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+            <hr />
+            <Typography
+              variant="body2"
+              align="center"
+              style={{ marginTop: "16px" }}
+            >
+              Dont have an account?{" "}
+              <Link href="/signUp" color="primary">
+                Sign Up
               </Link>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="body2" align="center">
-                Login as:
-              </Typography>
-              <br />
-              <Button variant="outlined" color="primary" fullWidth>
-                User
-              </Button>
-              <br />
-              <br />
-              <Button variant="outlined" color="primary" fullWidth>
-                Admin
-              </Button>
-            </Grid>
-            <Grid item xs={12}>
-              <Link to="/signUp">Doesnot have account? SignUp</Link>
-            </Grid>
-          </Grid>
-        </form>
-      </Paper>
-    </Container>
+            </Typography>
+          </Container>
+        </Paper>
+      </Grid>
+    </Grid>
   );
-};
+}
 
 export default Login;
