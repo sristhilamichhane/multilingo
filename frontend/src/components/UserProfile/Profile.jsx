d0faf2233620ac643e430ad64c9e6e85bb0b1d45
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-09-16 06:41:43 +0545

    feat: :sparkles: user's dummy profile

--- frontend/src/components/UserProfile/Profile.jsx
+++ frontend/src/components/UserProfile/Profile.jsx
@@ -1,6 +1,15 @@
-import { Box, Typography, styled } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Grid,
+  Paper,
+  Typography,
+  styled,
+} from "@mui/material";
 import Navbar from "../Navbar";
 import Sidebar from "../Sidebar";
+import "../../Pages/Home.css";
 
 const MainContent = styled(Box)(({ theme }) => ({
   flexGrow: 1,
@@ -11,6 +20,14 @@ const MainContent = styled(Box)(({ theme }) => ({
 }));
 
 const UserManagement = () => {
+  const user = {
+    name: "John Doe",
+    country: "United States",
+    address: "123 Main St",
+    email: "john@example.com",
+    phoneNumber: "+1 555-123-4567",
+  };
+
   return (
     <>
       <div className="bgColor">
@@ -20,7 +37,50 @@ const UserManagement = () => {
           <Sidebar />
           <MainContent>
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-              <Typography>Profile</Typography>
+              <Paper
+                elevation={3}
+                sx={{ padding: 2 }}
+                className="gradientLight"
+              >
+                <Typography variant="h4" gutterBottom>
+                  User Profile
+                </Typography>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} md={6}>
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="center"
+                    >
+                      <Avatar sx={{ width: 100, height: 100 }}>
+                        {/* You can add a user's profile picture here */}
+                      </Avatar>
+                      <Typography variant="h6" mt={1}>
+                        {user.name}
+                      </Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <Typography variant="body1">
+                      <strong>Name:</strong> {user.name}
+                    </Typography>
+                    <Typography variant="body1">
+                      <strong>Country:</strong> {user.country}
+                    </Typography>
+                    <Typography variant="body1">
+                      <strong>Address:</strong> {user.address}
+                    </Typography>
+                    <Typography variant="body1">
+                      <strong>Email:</strong> {user.email}
+                    </Typography>
+                    <Typography variant="body1">
+                      <strong>Phone Number:</strong> {user.phoneNumber}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Divider sx={{ my: 2 }} />
+                {/* Add more profile details or actions as needed */}
+              </Paper>
             </Box>
           </MainContent>
         </Box>
