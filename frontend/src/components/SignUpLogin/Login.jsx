1efb0d2224ca9b03405a9b4588494e79d60adc1f
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-09-17 07:28:52 +0545

    style: :art: beautifying login page

--- frontend/src/components/SignUpLogin/Login.jsx
+++ frontend/src/components/SignUpLogin/Login.jsx
@@ -27,65 +27,66 @@ function Login() {
   };
 
   return (
-    <Grid
-      container
-      justifyContent="center"
-      alignItems="center"
-      style={{ minHeight: "100vh" }} // Center vertically on the page
-    >
-      <Grid item xs={12} sm={8} md={6} lg={4}>
-        <Paper
-          elevation={3}
-          style={{ padding: "20px" }}
-          className="gradientLight"
-        >
-          <Container>
-            <Typography variant="h4" align="center" gutterBottom>
-              Login
-            </Typography>
-            <TextField
-              label="Username"
-              fullWidth
-              variant="outlined"
-              margin="normal"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <TextField
-              label="Password"
-              fullWidth
-              variant="outlined"
-              margin="normal"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <br />
-            <hr />
-            <br />
-            <Button
-              variant="contained"
-              color="primary"
-              fullWidth
-              onClick={handleLogin}
-            >
-              Login
-            </Button>
-            <hr />
-            <Typography
-              variant="body2"
-              align="center"
-              style={{ marginTop: "16px" }}
-            >
-              Dont have an account?{" "}
-              <Link href="/signUp" color="primary">
-                Sign Up
-              </Link>
-            </Typography>
-          </Container>
-        </Paper>
+    <div className="gradientLight">
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ minHeight: "100vh" }} // Center vertically on the page
+      >
+        <Grid item xs={12} sm={8} md={6} lg={4}>
+          <Paper
+            elevation={3}
+            style={{ padding: "20px", backgroundColor: "honeydew" }}
+          >
+            <Container>
+              <Typography variant="h4" align="center" gutterBottom>
+                Login
+              </Typography>
+              <TextField
+                label="Username"
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <TextField
+                label="Password"
+                fullWidth
+                variant="outlined"
+                margin="normal"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <br />
+              <hr />
+              <br />
+              <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                onClick={handleLogin}
+              >
+                Login
+              </Button>
+              <hr />
+              <Typography
+                variant="body2"
+                align="center"
+                style={{ marginTop: "16px" }}
+              >
+                Dont have an account?{" "}
+                <Link href="/signUp" color="primary">
+                  Sign Up
+                </Link>
+              </Typography>
+            </Container>
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 }
 
