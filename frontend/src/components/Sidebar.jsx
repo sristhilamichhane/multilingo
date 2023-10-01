f738d28ce4c1b5bac30721a268654ee28679732b
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-10-01 06:56:30 +0545

    docs: :construction_worker: adding proper navigation for Home page

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -98,7 +98,7 @@ export default function Sidebar() {
             disablePadding
             sx={{ display: "block" }}
             onClick={() => {
-              navigate("/");
+              navigate("/UserHome");
             }}
           >
             <ListItemButton
