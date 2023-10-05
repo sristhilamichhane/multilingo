3ea1cb4c6e310a4d18943d3293cad5526a81d585
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2023-10-05 08:14:06 +0545

    feat: :zap: adding new user for user management features

--- frontend/src/components/UserManagement/UserList.jsx
+++ frontend/src/components/UserManagement/UserList.jsx
@@ -12,6 +12,9 @@ import {
   Paper,
   TextField,
   Button,
+  Card,
+  CardContent,
+  Stack,
 } from "@mui/material";
 import {
   Delete as DeleteIcon,
@@ -25,6 +28,8 @@ const UserList = () => {
     { id: 2, name: "Jane Smith", points: 200 },
     { id: 3, name: "Bob Johnson", points: 150 },
   ]);
+
+  const [isAddUserCardOpen, setAddUserCardOpen] = useState(false);
 
   const [editingUserId, setEditingUserId] = useState(null);
   const [newUser, setNewUser] = useState({ id: "", name: "", points: "" });
@@ -61,6 +66,25 @@ const UserList = () => {
     setNewUser((prevUser) => ({ ...prevUser, [name]: value }));
   };
 
+  const handleAddUser = () => {
+    setAddUserCardOpen(true);
+  };
+  const handleAddUserSubmit = () => {
+    // Add the new user to the users array
+    setUsers((prevUsers) => [
+      ...prevUsers,
+      {
+        id: newUser.id, // You can generate a unique ID or use a different approach
+        name: newUser.name,
+        points: newUser.points,
+      },
+    ]);
+
+    // Close the add user card and reset the newUser state
+    setAddUserCardOpen(false);
+    setNewUser({ id: "", name: "", points: "" });
+  };
+
   return (
     <Box p={4}>
       <Typography variant="h4" gutterBottom>
@@ -86,23 +110,25 @@ const UserList = () => {
                 <TableCell>
                   {editingUserId === user.id ? (
                     <>
-                      <TextField
-                        name="name"
-                        value={newUser.name}
-                        onChange={handleInputChange}
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                      />
-                      <TextField
-                        name="points"
-                        value={newUser.points}
-                        onChange={handleInputChange}
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                        mt={1}
-                      />
+                      <Stack direction="row" gap={1}>
+                        <TextField
+                          name="name"
+                          value={newUser.name}
+                          onChange={handleInputChange}
+                          variant="outlined"
+                          size="small"
+                          fullWidth
+                        />
+                        <TextField
+                          name="points"
+                          value={newUser.points}
+                          onChange={handleInputChange}
+                          variant="outlined"
+                          size="small"
+                          fullWidth
+                          mt={1}
+                        />
+                      </Stack>
                       <Box mt={1}>
                         <Button
                           variant="contained"
@@ -152,10 +178,69 @@ const UserList = () => {
         variant="contained"
         startIcon={<AddIcon />}
         mt={4}
-        onClick={() => setEditingUserId(null)} // Set the editingUserId to null instead of 'new'
+        onClick={() => handleAddUser()}
       >
         Add User
       </Button>
+
+      {/* Add User Card */}
+      {isAddUserCardOpen && (
+        <Card mt={4}>
+          <CardContent>
+            <Typography variant="h6">Add New User</Typography>
+            <Stack direction="column" gap={1}>
+              <TextField
+                name="id"
+                label="User ID"
+                value={newUser.id}
+                onChange={handleInputChange}
+                variant="outlined"
+                size="small"
+                fullWidth
+                mb={1}
+              />
+              <TextField
+                name="name"
+                label="User Name"
+                value={newUser.name}
+                onChange={handleInputChange}
+                variant="outlined"
+                size="small"
+                fullWidth
+                mb={1}
+              />
+              <TextField
+                name="points"
+                label="User Points"
+                value={newUser.points}
+                onChange={handleInputChange}
+                variant="outlined"
+                size="small"
+                fullWidth
+                mb={1}
+              />
+            </Stack>
+            <Stack direction="row" marginTop={2}>
+              <Button
+                variant="contained"
+                size="small"
+                onClick={handleAddUserSubmit}
+              >
+                Save
+              </Button>
+              <Button
+                variant="contained"
+                size="small"
+                color="error"
+                onClick={() => setAddUserCardOpen(false)}
+                sx={{ marginLeft: 1 }}
+              >
+                Cancel
+              </Button>
+            </Stack>
+          </CardContent>
+        </Card>
+      )}
     </Box>
   );
 };
