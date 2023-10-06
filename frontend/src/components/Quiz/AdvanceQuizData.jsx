6af0d5399f65b4876b3f7fdb62992889b1662d97
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2023-10-06 12:33:19 +0545

    docs: :lipstick: added different Advance quiz content

---
+++ frontend/src/components/Quiz/AdvanceQuizData.jsx
@@ -0,0 +1,156 @@
+const QuizData = [
+  {
+    question:
+      "What is the Maithili translation of 'I need help with translation' ",
+    options: [
+      {
+        type: "text",
+        value: ["नमस्ते", "सुप्रभात", "अनुवाद मे हमरा मदद चाही ", "साधैं छ"],
+      },
+    ],
+    answer: 3,
+  },
+  {
+    question:
+      "Choose the correct translation for 'What are the popular tourist attractions' in Maithili.",
+    options: [
+      {
+        type: "text",
+        value: [
+          "माफ गर्नुहोस्",
+          "अहां कें धन्यवाद",
+          "पर्यटनक लोकप्रिय आकर्षण की अछि",
+          "हामीले छोडें",
+        ],
+      },
+    ],
+    answer: 3,
+  },
+  {
+    question:
+      "What is the Maithili word for 'Tell me about the local customs and traditions' ",
+    options: [
+      {
+        type: "text",
+        value: [
+          "फेरि भेटौला",
+          "धेरै धेरै धन्यवाद",
+          "अलविदा",
+          "स्थानीय रीति-रिवाज आ परंपराक बारे मे बताउ",
+        ],
+      },
+    ],
+    answer: 4,
+  },
+  {
+    question:
+      "Which option means 'What is the cost of living in this city?' in Maithili?",
+    options: [
+      {
+        type: "text",
+        value: [
+          "अहांक कोना छी",
+          "एहि शहर मे रहबाक की लागत?",
+          "कस्तो हो",
+          "तिमीलाई कस्तो छ",
+        ],
+      },
+    ],
+    answer: 2,
+  },
+  {
+    question:
+      "Choose the correct translation for 'Do you have any recommendations for places to visit?' in Maithili.",
+    options: [
+      {
+        type: "text",
+        value: [
+          "घुमबाक लेल कोनो सिफारिश अछि?",
+          "के गर्नु होला?",
+          "म कस्तो छु?",
+          "होइन, धन्यवाद",
+        ],
+      },
+    ],
+    answer: 1,
+  },
+  {
+    question:
+      "Which option means 'What is the weather like today?' in Maithili?",
+    options: [
+      {
+        type: "text",
+        value: [
+          "आइ मौसम केहन अछि?",
+          "तिमीलाई के हुँदैछ?",
+          "नजदीकी बैंक कतय अछि ?",
+          "होइन, धन्यवाद",
+        ],
+      },
+    ],
+    answer: 1,
+  },
+  {
+    question: "What is the Maithili translation for 'What are your hobbies'?",
+    options: [
+      {
+        type: "text",
+        value: [
+          "अहाँक कोन-कोन शौक अछि ?",
+          "नमस्ते",
+          "केही नभयो",
+          "मलाई निद्रा लाग्यो",
+        ],
+      },
+    ],
+    answer: 1,
+  },
+  {
+    question:
+      "Choose the correct translation for 'I'm interested in...' in Maithili.",
+    options: [
+      {
+        type: "text",
+        value: [
+          "हमरा रुचि अछि...",
+          "के गर्नु होला?",
+          "म कस्तो छु?",
+          "होइन, धन्यवाद",
+        ],
+      },
+    ],
+    answer: 1,
+  },
+  {
+    question: "What is the Maithili translation of 'What is this called'",
+    options: [
+      {
+        type: "text",
+        value: [
+          "एकरा की कहल जाइत छैक  ",
+          "माफ गर्नुहोस्",
+          "के गर्नु होला",
+          "हामीले छोडें",
+        ],
+      },
+    ],
+    answer: 1,
+  },
+  {
+    question:
+      "Choose the correct translation for 'Could you please recommend a good restaurant?",
+    options: [
+      {
+        type: "text",
+        value: [
+          "फेरि भेटौला",
+          "धेरै धेरै धन्यवाद",
+          "नीक रेस्टोरेंटक सिफारिश क सकैत छी?",
+          "शुभकामना",
+        ],
+      },
+    ],
+    answer: 3,
+  },
+];
+export default QuizData;
